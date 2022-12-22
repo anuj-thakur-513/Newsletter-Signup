7ce82785b023c7a405fd36142a791b65a129ed36
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-12-22 19:36:18 +0530

    sending data to mailchimp server

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const request = require("request");
+const client = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 // creating a port to host the server
 const PORT = process.env.PORT || 3000;
+
+// setting info for client(mailchimp)
+client.setConfig({
+  apiKey: "925c0ce9a9c6e7ff6ca2753acd78018c-us17",
+  server: "us17",
+});
 
 // using body-parser
 app.use(express.json());
@@ -19,14 +25,40 @@ app.get("/", (req, res) => {
 
 // setting the post route
 app.post("/", (req, res) => {
-  let firstName = req.body.fName;
-  let lastName = req.body.lName;
-  let email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(`${firstName} ${lastName} ${email}`);
+  // JS user object
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  // adding data to the mailchimp server asynchronously
+  async function run() {
+    const response = await client.lists.addListMember("6ddfbc491d", {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+    console.log(response);
+  }
+
+  run().catch((e) => res.sendFile(__dirname + "/failure.html"));
 });
 
 // starting the server
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
+
+// API Key
+// 925c0ce9a9c6e7ff6ca2753acd78018c-us17
+
+// List/Audience ID
+// 6ddfbc491d
